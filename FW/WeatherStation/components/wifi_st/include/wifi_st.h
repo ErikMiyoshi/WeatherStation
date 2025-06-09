78f95ae673b2a2b8390bbd6277256efa92540e83
Erik <erikmiyoshi@hotmail.com>
2025-06-08 21:09:58 -0300

    add functions to connect to wifi

---
+++ FW/WeatherStation/components/wifi_st/include/wifi_st.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+    int32_t event_id, void* event_data);
+
+void wifi_init_sta(void);
